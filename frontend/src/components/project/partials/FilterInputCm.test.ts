72e5b268fcf83ef724875da64e26429bd0867e57
Dominik Pschenitschni <6173598+dpschen@users.noreply.github.com>
2025-06-11 10:57:26 +0200

    test: add basic FilterInputCm sync tests

--- frontend/src/components/project/partials/FilterInputCm.test.ts
+++ frontend/src/components/project/partials/FilterInputCm.test.ts
@@ -1,7 +1,33 @@
 import {describe, it, expect} from 'vitest'
+import {mount} from '@vue/test-utils'
+import {nextTick} from 'vue'
+import FilterInputCm from './FilterInputCm.vue'
 
 describe('FilterInputCm', () => {
-it.skip('placeholder test', () => {
-expect(true).toBe(true)
-})
+       it('syncs prop changes to the internal query', async () => {
+               const wrapper = mount(FilterInputCm, {props: {modelValue: 'foo'}})
+
+               await nextTick()
+
+               const query = (wrapper.vm as any).$?.setupState.query
+
+               expect(query.value).toBe('foo')
+
+               await wrapper.setProps({modelValue: 'bar'})
+               await nextTick()
+
+               expect(query.value).toBe('bar')
+       })
+
+       it('emits updates when the query changes', async () => {
+               const wrapper = mount(FilterInputCm, {props: {modelValue: 'foo'}})
+
+               await nextTick()
+
+               const query = (wrapper.vm as any).$?.setupState.query
+               query.value = 'baz'
+               await nextTick()
+
+               expect(wrapper.emitted('update:modelValue')).toEqual([['baz']])
+       })
 })
